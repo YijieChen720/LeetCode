a56abfe4a68c61efbda382400d99c7cc8850816c
YijieChen720 <87942117+YijieChen720@users.noreply.github.com>
2021-10-14 00:21:12 -0400

    -updated listnode class

--- List/src/ReverseLinkedList.java
+++ List/src/ReverseLinkedList.java
@@ -21,14 +21,6 @@ public class ReverseLinkedList {
         return head;
     }
 
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
     public static void main(String[] args) {
         ListNode head;
         head = new ListNode(1, new ListNode(2, new ListNode(3, null)));
--- List/src/SwapNodesInPairs.java
+++ List/src/SwapNodesInPairs.java
@@ -38,11 +38,5 @@ public class SwapNodesInPairs {
         return head;
     }
 
-    public static class ListNode {
-        int val;
-        ReverseLinkedList.ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ReverseLinkedList.ListNode next) { this.val = val; this.next = next; }
-    }
+
 }
