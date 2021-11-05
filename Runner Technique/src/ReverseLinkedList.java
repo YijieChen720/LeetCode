e8321b078c63361f869ad33895a90db3a62a4391
YijieChen720 <87942117+YijieChen720@users.noreply.github.com>
2021-11-04 22:03:58 -0400

    19. Remove Nth Node From End of List

--- Runner Technique/src/ReverseLinkedList.java
+++ Runner Technique/src/ReverseLinkedList.java
@@ -1,13 +1,4 @@
 public class ReverseLinkedList {
-
-    private class ListNode {
-       int val;
-       ListNode next;
-       ListNode() {}
-       ListNode(int val) { this.val = val; }
-       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
     public ListNode reverseList(ListNode head) {
         if (head == null) {
             return null;
